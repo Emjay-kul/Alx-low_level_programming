2be7f4cfa10a59bc8840fadd38a693bfcf49db9a
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-03 05:41:22 -0700

    stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 #include<stdio.h>
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Kerpar, 2015-"
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-"
 		"10-19\n");
 	return (1);
 }
