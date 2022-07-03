405cd4fc1858d2d4b61c426e91e6d39536164f26
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-03 04:34:41 -0700

    stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (success)
+ */
+
+#include<stdio.h>
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
