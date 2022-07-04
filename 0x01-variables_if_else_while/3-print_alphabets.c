19fbc8d54bd425e28f74445fa72c4814c20ecfd0
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-04 09:40:37 -0700

    az

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x;
+
+	for (int x = 'A'; x <= 'Z'; x++)
+	{
+		int lower = tolower(x);
+
+		putchar(lower);
+
+		putchar(x);
+
+	}
+	putchar('\n');
+
+	return (0);
+
