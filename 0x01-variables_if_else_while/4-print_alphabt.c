83c9cb51b4335037c4fe9065ad3e27b610d78339
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-04 10:11:58 -0700

    upper

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		int lower = tolower(x);
+
+		putchar(lower);
+
+		if (x == q || x == e);
+		    continue
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
