fb875fb3b8663f7412135b6f91023a8a8af07610
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-04 11:34:26 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	int x;
+
+	for (x == 0; x < 10; x++)
+	{
+		printf("%i", x);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
