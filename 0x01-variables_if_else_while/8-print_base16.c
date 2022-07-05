a232f064aa090f706de4a2c88e057468e8d93a0e
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-04 17:53:23 -0700

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+
+	for (letter = 'A'; letter <= 'F'; letter++)
+	{
+		int lower = tolower(letter);
+
+		putchar(lower);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
+
