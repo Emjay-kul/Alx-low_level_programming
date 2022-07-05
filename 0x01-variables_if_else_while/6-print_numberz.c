817b41a57f16b28d9530fadbaa9b12f70503a3c3
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-04 17:13:20 -0700

    putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	for ( i = 0; i < 10; i++)
+	{
+		int putchar(int i);
+	}
+
+	return (0);
+}
