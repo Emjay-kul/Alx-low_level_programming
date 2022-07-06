b713d21dd62a43522a43056fb734aee2e8c3a584
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-06 15:05:19 -0700

    k

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,14 +11,14 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		putchar('0');
-		
+
 		for (mult = 1; mult <= 9; mult++)
 		{
 			putchar(',');
 			putchar(' ');
 			
 			prod = num * mult;
-			
+
 			if (prod <= 9)
 				putchar(' ');
 			else
