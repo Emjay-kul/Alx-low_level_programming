f662ad5ea54357e2b53aa902f40dd956ee209add
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-06 02:35:48 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_sign -nfunction that prints the sign of a given number
+ *
+ * @n: given parameter
+ *
+ * Return: 0 if given number is 0 and 1 if given number is positive or negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+
+		return (1);
+	}
+	else if (n < 0)
+	{
+		putchar('-');
+
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+
+		return (0);
+	}
+}
