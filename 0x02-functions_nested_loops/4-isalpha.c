e2a5c1c2f253a2a8bfd18ec7ae2d99bd99485806
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-06 02:05:36 -0700

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isalpha - function that checks for alphabets
+ *
+ * @c: parameters given
+ *
+ * Return: 1 if alphabet is present and 0 if alphabet is absent
+ */
+
+int _isalpha(int c)
+{
+	int c;
+
+	if ((c = 'a'; c <= 'z') || (c = 'A'; c <= 'Z'));
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
