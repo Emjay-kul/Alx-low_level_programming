61b9bfcfa2a18cfc38aa329be8788f36d3452093
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-06 11:54:38 -0700

    k

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - code to print absolute value 
+ *
+ * Return: The absolute value
+ */
+int _abs(int)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+		return (-n);
+}
+
+
