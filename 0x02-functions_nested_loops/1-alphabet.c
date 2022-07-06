36fcfc75a4cdcfa08a1ba0edd27ff9d45bd856e2
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-05 20:30:30 -0700

    l

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - prints the alphabet in lower case
+ *  followed by a new line
+ */
+
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x = 'z'; x++)
+	{
+		putchar(x);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
