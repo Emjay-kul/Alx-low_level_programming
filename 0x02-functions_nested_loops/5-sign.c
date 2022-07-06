670eb2eec32a52496cae918f6d913bc416071dc3
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-06 09:28:21 -0700

    k

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,9 +12,9 @@
 
 int print_sign(int n)
 {
-	if (n < 0)
+	if (n > 0)
 	{
-		putchar('-');
+		putchar('+');
 		return (1);
 	}
 	else if (n == 0)
@@ -22,10 +22,10 @@ int print_sign(int n)
 		putchar('0');
 		return (0);
 	}
-	else if (n > 0)
+	else if (n < 0)
 	{
-		putchar('+');
-		return (0);
+		putchar('-');
+		return (-1);
 	}
 	return (0);
 }
