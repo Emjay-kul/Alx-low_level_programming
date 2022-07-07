563f13666a9a07b2aabeb6f35a1c3f7c4fce7a66
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-06 20:57:50 -0700

    h

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,9 +3,13 @@
 /**
  * add - function that adds two integers
  *
+ * @num1: First integer
+ *
+ * @num2: second integer
+ *
  * Return: the sum of both int
  */
-int add(int, int)
+int add(int num1, int num2)
 {
-	return (int  + int);
+	return (num1 + num2);
 }
