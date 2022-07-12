1d885ebc138d1eb81871b9c8b4376b76456fb400
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-12 06:31:16 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcpy - copies a string from src to dest
+ *
+ * @dest: pointer value
+ *
+ * @src pointer value
+ *
+ * Return: the pinter to dest
+ */
+char _strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while(src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	return (dest);
+}
