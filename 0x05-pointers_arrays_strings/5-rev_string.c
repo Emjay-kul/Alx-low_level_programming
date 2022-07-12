1e6c0c9e0c5415691419f0f0ff9b60fa39c065ad
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-12 05:17:47 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * rev_string - print in reverse
+ *
+ * @s: string value
+ *
+ * Return: Always 0
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int index = 0;
+	char tmp;
+
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= len / 2; index--)
+	{
+		tmp = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = tmp;
+}
