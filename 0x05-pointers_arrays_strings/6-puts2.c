41b222697c68b84234d7c092cd73b77ebaf0581c
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-12 05:30:31 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts2 - prints a string to stdout
+ *
+ * @str: character value
+ *
+ * Return: Always 0
+ */
+void _puts2(char *str)
+{
+	int len = 0;
+	int index = 0;
+
+	while (*str[index++])
+		len++;
+
+	for (index = 0; index < len; index += 2)
+		putchar(*str[index]);
+
+	putchar('\n');
+
+}
