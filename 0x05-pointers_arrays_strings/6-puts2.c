88159538b1f3afa4e06c0b307f8c72a997e41534
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-12 05:34:36 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,7 +7,7 @@
  *
  * Return: Always 0
  */
-void _puts2(char *str)
+void puts2(char *str)
 {
 	int len = 0;
 	int index = 0;
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,4 +4,4 @@ void _puts(char *str);
 int _strlen(char *s);
 void print_rev(char *s);
 void rev_string(char *s);
-void _puts2(char *str);
+void puts2(char *str);
