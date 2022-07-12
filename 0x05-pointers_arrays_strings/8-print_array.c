dd5431808a934311480e041be1e46eaf46401ecb
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-12 06:22:02 -0700

    some array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,7 +16,12 @@ void print_array(int *a, int n)
 	for (index = 0; index < n; index++)
 	{
 		printf("%d", a[index]);
+
+		if (index = n - 1)
+			continue;
+
 		printf(", ");
 	}
+	printf("\n");
 }
 
