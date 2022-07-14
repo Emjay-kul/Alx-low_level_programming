8b2054874a206aaca0cb770f6cea10467d7878b3
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-14 09:37:01 -0700

    d

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,19 +1,31 @@
 #include "main.h"
 /**
- * cap_string - changes lowercase letters to uppercase
+ * cap_string - reverses the content of an integer array
  *
- * @s: string to change
+ * @s: string array
  *
  * Return: Address of s
  */
-char *string_toupper(char *s)
+char *cap_string(char *s)
 {
 	int i = 0;
 
-	while(*(s + i))
+	while(s[i] != '\0')
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
-			*(s + i) -= 'a' - 'A';
+		if (s[0] <= 122 && s[0] >= 97)
+		{
+			s[0] = s[0] - 32;
+		}
+		if (s[i] == 32 || s[i] == 46 || s[i] == '\t' ||
+			s[i] == '\n' || s[i] == 44 || s[i] == 59 ||
+				s[i] == '!' || s[i] == '?' || s[i] == '(' ||
+				s[i] == ')' || s[i] == '{' || s[i] == '}')
+		{
+			if (s[i + 1] <= 122 && s[i + 1] >= 97)
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
 		i++;
 	}
 	return (s);
