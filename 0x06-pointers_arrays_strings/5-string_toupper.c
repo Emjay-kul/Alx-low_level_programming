90647c5c80c317bf63cfc295cec5d181b3bd74d9
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-14 08:54:11 -0700

    d

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,10 +10,10 @@ char *string_toupper(char *s)
 {
 	int i = 0;
 
-	while(s[i] != '\0')
+	while(*(s + i))
 	{
-		if ((s[i] >= 97) && (s[i] <= 122))
-			s[i] = s[i] - 32;
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) -= 'a' - 'A';
 		i++;
 	}
 	return (s);
