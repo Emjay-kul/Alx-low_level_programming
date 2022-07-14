4e55068b0585a42c3371a2ab7cbbc2eb65cabb03
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-14 07:03:26 -0700

    s

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,23 +11,15 @@
 void reverse_array(int *a,int n)
 {
 	int i = 0;
-	int j = 0;
-	int aux = 0;
-	int *init, *final;
-
-	init = a;
-	final = a;
+	int aux;
+	
+	
 	n -= 1;
 
-	while (i < n)
-		i += 1;
-
-	while (j <= 1)
+	while (i <= n)
 	{
-		aux = *(init + j);
-		*(init + j) = *(final + i);
-		*(final + i) = aux;
-		j++;
-		i--;
+		aux = a[i];
+		a[i++] = a[n];
+		a[n--] = aux;
 	}
 }
