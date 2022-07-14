3a4b55b70543c4dfde761a8d5489b33bba4f6514
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-14 07:23:56 -0700

    f

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,19 +11,11 @@
 char _strcmp(char *s1, char *s2)
 {
 	int i;
-	int aux;
-
-	for (i = 0; s1[i] != '\0'; i++)
+	
+	for (i = 0; s1[i] != '\0'; && s2[i] != '\0'; i++)
 	{
-		if (s1[i] == s2[i])
-		{
-			aux = (s1[i] - '0') - (s2[i] - '0');
-		}
-		else
-		{
-			aux = (s1[i] - '0') - (s2[i] - '0');
-			break;
-		}
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 	}
-	return (result);
+	return (0);
 }
