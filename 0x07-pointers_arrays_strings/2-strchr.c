843a0cf9768b2ee523e97b58d66dbf6d19623d50
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-19 15:24:03 -0700

    string check

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr - returns a pointer once c is encountered in the string s
+ *
+ * @s: string pointer
+ *
+ * @c: character whose occurrence in the string is being verified
+ *
+ * Return: A pointer to string s
+ */
+char *_strchr(char *s, char c)
+{
+	while(*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
