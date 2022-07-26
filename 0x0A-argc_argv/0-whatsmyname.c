e77ed3c1e43df3733defc7359b9e0f89bca970aa
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-07-26 11:48:20 -0700

    args

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Return: Always 0
+ */
+void main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s", argv[i])
+	}
+	printf("\n");
+}
