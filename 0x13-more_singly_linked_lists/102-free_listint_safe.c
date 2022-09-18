ec984f3131741f396db2c77373282b0c51e82014
Emjay-kul <nwokoyemaryjamaryjane@gmail.com>
2022-09-18 14:46:26 -0700

    free list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -15,14 +15,14 @@ listint_t **_ra(listint_t **list, size_t size, listint_t *new)
 	size_t i;
 
 	newlist = malloc(size * sizeof(listint_t *));
-       if (newlist == NULL)
-       {
-	       free(list);
-	       exit(98);
-       }
-       for (i = 0; i < size - 1; i++)
-	       newlist[i] = list[i];
-       newlist[i] = new;
-       free(list);
-       return (newlist);
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0; i < size - 1; i++)
+		newlist[i] = list[i];
+	newlist[i] = new;
+	free(list);
+	return (newlist);
 }
